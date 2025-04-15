4bae88b71544f9a07e4f00f238694e0002a427f8
CarlAndreDiomon <dcarlandrerafols@gmail.com>
2025-04-15 09:10:06 +0800

    Add users logs schema and model

--- src/Model/usersSchema.js
+++ src/Model/usersSchema.js
@@ -1,5 +1,6 @@
 import mongoose from "mongoose";
 
+// Student Schema
 const studentSchema = new mongoose.Schema({
     fullName: {
         type: String,
@@ -15,9 +16,31 @@ const studentSchema = new mongoose.Schema({
         required: true,
     },
 });
-
+// Create the Student model
 const Student = mongoose.model("Student", studentSchema);
-
+// Student Login Log Schema
+const studentLoginLogSchema = new mongoose.Schema({
+    studentId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Student",
+        required: true,
+    },
+    fullName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    loginTime: {
+        type: Date,
+        default: Date.now,
+    },
+});
+// Create the StudentLoginLog model
+const StudentLoginLog = mongoose.model("StudentLoginLog", studentLoginLogSchema);
+//  Teacher Schema
 const teacherSchema = new mongoose.Schema({
     fullName: {
         type: String,
@@ -33,7 +56,30 @@ const teacherSchema = new mongoose.Schema({
         required: true,
     },
 });
-
+// Create the Teacher model
 const Teacher = mongoose.model("Teacher", teacherSchema);
+// Teacher Login Log Schema
+const teacherLoginLogSchema = new mongoose.Schema({
+    teacherId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Teacher",
+        required: true,
+    },
+    fullName: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+    },
+    loginTime: {
+        type: Date,
+        default: Date.now,
+    },
+});
+// Create the TeacherLoginLog model
+const TeacherLoginLog = mongoose.model("TeacherLoginLog", teacherLoginLogSchema);
+// Export the models
+export { Student, Teacher, StudentLoginLog, TeacherLoginLog };
 
-export { Student, Teacher };
