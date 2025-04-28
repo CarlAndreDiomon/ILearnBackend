1e343dd513ac6034f09c9083c73d0a77e9058820
CarlAndreDiomon <dcarlandrerafols@gmail.com>
2025-04-28 11:25:11 +0800

    fixing token request

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -5,11 +5,8 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const protect = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-
-  if (authHeader && authHeader.startsWith('Bearer ')) {
-    const token = authHeader.split(' ')[1];
-
+  const token = req.cookies.jwt
+  if (token) {
     try {
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
       req.user = decoded; // You can access req.user in protected routes
