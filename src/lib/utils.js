e70cbbcdc3080c9421368fceb0cd7eaf49c28696
CarlAndreDiomon <dcarlandrerafols@gmail.com>
2025-04-28 11:24:29 +0800

    fixing res.cookie for development and production mode

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -9,7 +9,7 @@ export const generateToken = (userId,role, res) => {
     res.cookie("jwt", token, {
         maxAge: 30 * 24 * 60 * 60 * 1000, // MS
         httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-        sameSite: "Strict", // CSRF attacks cross-site request forgery attacks
+        sameSite: process.env.NODE_ENV !== "development" ? "None" : "Strict", // CSRF attacks cross-site request forgery attacks
         secure: process.env.NODE_ENV !== "development",
     });
 
