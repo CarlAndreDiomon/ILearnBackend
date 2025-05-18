982bc6d6119cb22c5912b710af6afdc9c0c5a7eb
CarlAndreDiomon <dcarlandrerafols@gmail.com>
2025-05-18 21:48:56 +0800

    fix the error in token

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -9,7 +9,7 @@ export const generateToken = (userId,role, res) => {
     res.cookie("jwt", token, {
         maxAge: 30 * 24 * 60 * 60 * 1000, // MS
         httpOnly: true, // prevent XSS attacks cross-site scripting attacks
-        sameSite: true, // CSRF attacks cross-site request forgery attacks
+        sameSite: 'None', // CSRF attacks cross-site request forgery attacks
         secure: true,
     });
 
