3391a4428f3d70a52c7f1bf3362366faa23ff819
CarlAndreDiomon <dcarlandrerafols@gmail.com>
2025-05-18 22:01:47 +0800

    Fixing logout logic

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -163,16 +163,21 @@ export const loginTeacher = async(req, res) => {
         return res.status(400).send({message: "Internal server error"});
     }
 };
-
 export const logout = (req, res) => {
-    try {
-      res.cookie("jwt", "", { maxAge: 0 });
-      res.status(200).json({ message: "Logged out successfully" });
-    } catch (error) {
-      console.log("Error in logout controller", error.message);
-      res.status(500).json({ message: "Internal Server Error" });
-    }
-  };
+  try {
+    res.cookie("jwt", "", {
+      maxAge: 0,
+      httpOnly: true,
+      sameSite: "None",
+      secure: true,
+    });
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (error) {
+    console.log("Error in logout controller", error.message);
+    res.status(500).json({ message: "Internal Server Error" });
+  }
+};
+
 
 export const changeProfile = async (req, res) => {
     const { fullName, email } = req.body;
