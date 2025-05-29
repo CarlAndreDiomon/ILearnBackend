4cd9f402444a929f6dbe23a3c3d787cd371dde84
CarlAndreDiomon <dcarlandrerafols@gmail.com>
2025-05-30 07:44:26 +0800

    Fixing

--- src/routes/moduleRoutes.js
+++ src/routes/moduleRoutes.js
@@ -1,12 +1,12 @@
 import express from 'express';
 import { downloadFiles, getFiles, uploadFiles } from '../controllers/moduleController.js';
-import { isStudent, isTeacher } from '../middleware/roleCheckMiddleware.js';
+import { isStudent} from '../middleware/roleCheckMiddleware.js';
 import protect from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
 // Route to get modules grouped by grade
-router.post('/upload', protect, isTeacher, uploadFiles);
+router.post('/upload', protect, uploadFiles);
 
 // Route to get modules grouped by grade
 router.get('/files', protect, getFiles);
