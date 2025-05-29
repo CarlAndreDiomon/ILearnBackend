9ac92449f3d0bd25bde753940498b9710024279f
CarlAndreDiomon <dcarlandrerafols@gmail.com>
2025-05-30 00:49:18 +0800

    Fix

--- src/config/firebaseConfig.mjs
+++ src/config/firebaseConfig.mjs
@@ -2,15 +2,14 @@ import admin from 'firebase-admin';
 import dotenv from 'dotenv';
 dotenv.config();
 
-if (!process.env.FIREBASE_SERVICE_ACCOUNT_KEY) {
-  throw new Error('FIREBASE_SERVICE_ACCOUNT_KEY is not defined in the environment.');
-}
 
+// Read and parse the JSON key
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
+
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  storageBucket: 'i-learn-app-a0baa.appspot.com', // fix domain: should end with .app**spot**.com
+  storageBucket: 'i-learn-app-a0baa.firebasestorage.app', // replace with yours
 });
 
 const bucket = admin.storage().bucket();
